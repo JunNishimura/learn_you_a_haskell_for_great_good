92b13e2366c21d369ab349b4ac0e4981b56bf886
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-17 21:12:03 +0900

    chap6: some higher-orderism

--- chap6/higher_order_functions.hs
+++ chap6/higher_order_functions.hs
@@ -9,3 +9,14 @@ dividedByTen = (/10)
 
 isUpperAlphanum :: Char -> Bool
 isUpperAlphanum = (`elem` ['A'..'Z'])
+
+applyTwice :: (a -> a) -> a -> a
+applyTwice f x = f (f x)
+
+zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+zipWith' _ [] _ = []
+zipWith' _ _ [] = []
+zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+flip' :: (a -> b -> c) -> (b -> a -> c)
+flip' f x y = f y x
