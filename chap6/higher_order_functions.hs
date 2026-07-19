a04f9a24b4eb4b420430dbbe6aa067dd9f9a744f
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-19 18:06:09 +0900

    chap6: fold

--- chap6/higher_order_functions.hs
+++ chap6/higher_order_functions.hs
@@ -1,3 +1,4 @@
+import Data.Foldable1 (Foldable1(fold1))
 multThree :: (Num a) => a -> a -> a -> a
 multThree x y z = x * y * z
 
@@ -57,3 +58,37 @@ numLonnChains' = length (filter (\xs -> length xs > 15) (map chain [1..100]))
 
 flip'' :: (a -> b -> c) -> b -> a -> c
 flip'' f = \x y -> f y x
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+-- elem' y ys = foldl (\acc x -> acc || y == x) False ys
+elem' y ys = foldl (\acc x -> if y == x then True else False) False ys
+
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
+
+containsThree :: [Int] -> Bool
+containsThree = foldr (\x rest -> if x == 3 then True else rest) False
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldr1 (\x acc -> if x > acc then x else acc)
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
+
+product' :: (Num a) => [a] -> a
+product' = foldr1 (*)
+
+filter'' :: (a -> Bool) -> [a] -> [a]
+filter'' p = foldr (\x acc -> if p x then x : acc else acc) []
+
+head' :: [a] -> a
+head' = foldr1 (\x _ -> x)
+
+last' :: [a] -> a
+last' = foldl1 (\_ x -> x)
+
+sqrtSums :: Int
+sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
