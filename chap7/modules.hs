b0b4d7b39455c7953f8d555e071179a4824e46f2
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-25 22:29:54 +0900

    chap7: Data.Char

--- chap7/modules.hs
+++ chap7/modules.hs
@@ -1,6 +1,7 @@
 import Data.List
 -- import Data.List hiding (nub)
 import qualified Data.Map as M
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -9,3 +10,12 @@ search :: (Eq a) => [a] -> [a] -> Bool
 search needle haystack =
     let nlen = length needle
     in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
+
+encode :: Int -> String -> String
+encode shift msg =
+    let ords = map ord msg
+        shifts = map (+ shift) ords
+    in map chr shifts
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
