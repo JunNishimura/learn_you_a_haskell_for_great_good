78df4b516dbf76dd7a9036e4fbe40690b87e1781
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-25 21:52:11 +0900

    chap7: Data.List

--- chap7/modules.hs
+++ chap7/modules.hs
@@ -1,6 +1,11 @@
-import Data.List (nub, sort)
+import Data.List
 -- import Data.List hiding (nub)
 import qualified Data.Map as M
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+search :: (Eq a) => [a] -> [a] -> Bool
+search needle haystack =
+    let nlen = length needle
+    in foldl (\acc x -> if take nlen x == needle then True else acc) False (tails haystack)
