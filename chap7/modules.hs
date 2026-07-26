2eb96daee0182e82a4e6112a155b6130a0734da7
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-26 23:21:56 +0900

    chap7: make our own modules

--- chap7/modules.hs
+++ chap7/modules.hs
@@ -55,3 +55,4 @@ text1 = "I just had an anime dream. Anime... Reality... Are they so different?"
 text2 = "The old man left his garbage can out and now his trash is all over my lawn!" 
 
 setNub xs = S.toList $ S.fromList xs
+
