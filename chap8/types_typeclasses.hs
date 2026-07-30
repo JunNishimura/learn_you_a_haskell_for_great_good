82854a19faf0a19e19f1fc38bb640ff238ccd80e
Jun Nishimura <n.junjun0303@gmail.com>
2026-07-31 00:09:12 +0900

    chap8: record syntax

--- chap8/types_typeclasses.hs
+++ chap8/types_typeclasses.hs
@@ -26,3 +26,20 @@ surface (Rectangle (Point x1 y1) (Point x2 y2)) = (abs $ x2 - x1) * (abs $ y2 - 
 nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x + a) (y + b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1 + a) (y1 + b)) (Point (x2 + a) (y2 + b))
+
+-- data Person = Person String String Int Float String String deriving (Show)
+data Person = Person {
+    firstName :: String,
+    lastName :: String,
+    age :: Int,
+    height :: Float,
+    phoneNumber :: String,
+    flavor :: String
+} deriving (Show)
+
+-- data Car = Car String String Int deriving (Show)
+data Car = Car {
+    company :: String,
+    model :: String,
+    year :: Int
+} deriving (Show)
