ae7689e9fc8e6a69fdebed38b8a2a827ce84d3b3
Jun Nishimura <n.junjun0303@gmail.com>
2026-08-03 23:07:46 +0900

    chap8: type parameters

--- chap8/types_typeclasses.hs
+++ chap8/types_typeclasses.hs
@@ -38,8 +38,35 @@ data Person = Person {
 } deriving (Show)
 
 -- data Car = Car String String Int deriving (Show)
-data Car = Car {
-    company :: String,
-    model :: String,
-    year :: Int
+-- data Car = Car {
+--     company :: String,
+--     model :: String,
+--     year :: Int
+-- } deriving (Show)
+
+data Car a b c = Car {
+    company :: a,
+    model :: b,
+    year :: c
 } deriving (Show)
+
+-- tellCar :: Car -> String
+-- tellCar (Car {company = c, model = m, year = y}) = "This" ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+tellCar :: (Show a) => Car String String a -> String
+tellCar (Car {company = c, model = m, year = y}) = "This" ++ c ++ " " ++ m ++ " was made in " ++ show y
+
+data Maybe a = Nothing | Just a
+
+-- data (Ord k) = Map k v
+
+data Vector a = Vector a a a deriving (Show)
+
+vplus :: (Num t) => Vector t -> Vector t -> Vector t
+(Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
+
+vectMult :: (Num t) => Vector t -> t -> Vector t
+(Vector i j k) `vectMult` m = Vector(i*m) (j*m) (k*m)
+
+scalarMult :: (Num t) => Vector t -> Vector t -> t
+(Vector i j k) `scalarMult` (Vector l m n) = i*l + j*m + k*n
