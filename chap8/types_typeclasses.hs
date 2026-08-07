41da6e64a98dfe35b23c8641e6ca510d427b8d18
Jun Nishimura <n.junjun0303@gmail.com>
2026-08-07 22:31:09 +0900

    chap8: kinds

--- chap8/types_typeclasses.hs
+++ chap8/types_typeclasses.hs
@@ -229,3 +229,16 @@ instance Main.Functor Tree where
 -- instance Main.Functor (Either a) where
 --     fmap f (Main.Right x) = Main.Right (f x)
 --     fmap f (Main.Left x) = Main.Left x
+
+class Tofu t where
+    tofu :: j a -> t a j
+
+data Frank a b = Frank {frankField :: b a} deriving (Show)
+
+instance Tofu Frank where
+    tofu x = Frank x
+
+data Barry t k p = Barry { yabba :: p, dabba :: t k}
+
+instance Main.Functor (Barry a b) where
+    fmap f (Barry {yabba = x, dabba = y}) = Barry { yabba = f x, dabba = y}
